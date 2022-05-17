9665d1903b0208e1ba72f39adaf5a9b512df6003
Nadya Williams <nadya.williams@gmail.com>
2022-05-17 16:44:18 -0700

    this rename is due to the redoing all RPMS names to keep with naming schema. This filter is used only in pytorch-cuda build

---
+++ yamlspecs/filter-requires-pytorch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# remove erroneous requirements
+# used when building pytorch-cuda 
+
+/usr/lib/rpm/find-requires $* | sed  \
+    -e '/(libnvToolsExt.so.1)(64bit)/d' \
+    -e '/(libcusparse.so.10)(64bit)/d' \
+    -e '/(libcurand.so.10)(64bit)/d' \
+    -e '/(libnvrtc.so.10.1)(64bit)/d' \
+    -e '/(libcufft.so.10)(64bit)/d' \
+    -e '/(libcudart.so.10.1)(64bit)/d'
